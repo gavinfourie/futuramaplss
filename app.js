8317c2640b6dfb42315aec73ee1d0ce1bb0b4bc7
Gavin Fourie <gavinfourie07@gmail.com>
2020-06-22 20:29:53 +0200

    Bug fixes

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.get('/start', (req, res) => {
             res.render('response', { data: response.data })
         })
         .catch(function (error) {
-            console.log('errorFG', error.data)
+            console.log('errorFG', error.response)
         })
 })
 
