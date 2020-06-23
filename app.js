cf366299a5aea526634f205174965144cf41fd80
Gavin Fourie <gavinfourie07@gmail.com>
2020-06-23 18:15:41 +0200

    Bug fixes

--- app.js
+++ app.js
@@ -44,8 +44,8 @@ app.get('/start', (req, res) => {
     console.log("Token: ", token)
     zoho.get('view')
         .then(function (response) {
-            console.log('responseFG:', response.data )
-            res.render('response', { data: response.data })
+            console.log('responseFG:', response.data.data )
+            res.render('response', { data: response.data.data })
         })
         .catch(function (error) {
             console.log('errorFG', error.response)
