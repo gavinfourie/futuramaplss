4d2de20b2fee1f144e77b74b04ef8d45688afb3f
Gavin Fourie <gavinfourie07@gmail.com>
2020-07-04 16:43:46 +0200

    Fixed bugs

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/get', (req, res) => {
-    worksheet_name = 'Lite Optec'
+    worksheet_name = 'LiteOptec'
     res.redirect('https://accounts.zoho.com/oauth/v2/auth?response_type=code&client_id=1000.MAUUUZO4JJ0D5UOS7NA1XJA6EIJADH&scope=ZohoSheet.dataAPI.READ&redirect_uri=https://futurama-app.herokuapp.com/redirect')
 })
 
