02a201d8255fe5a7bde27d283b28e5ee42e7c699
Gavin Fourie <gavinfourie07@gmail.com>
2020-07-05 17:23:26 +0200

    Fixed bugs

--- suppliers/liteoptec.js
+++ suppliers/liteoptec.js
@@ -6,10 +6,12 @@ let workbook_name = 'Lite Optec'
 let token = null
 let oldPrices = []
 let newPrices= []
+let priceChanges = []
 
 router.get('/', (req, res) => {
     oldPrices = []
     newPrices = []
+    priceChanges = []
     res.redirect('https://accounts.zoho.com/oauth/v2/auth?response_type=code&client_id=1000.MAUUUZO4JJ0D5UOS7NA1XJA6EIJADH&scope=ZohoSheet.dataAPI.READ&redirect_uri=https://futurama-app.herokuapp.com/liteoptec/redirect')
 })
 
@@ -33,7 +35,6 @@ router.get('/workbook', (req, res) => {
     })
     zoho.get('workbooks?method=workbook.list')
         .then(function (response) {
-            console.log(response.data.workbooks)
             let workbooks = response.data.workbooks
             for (let index = 0; index < workbooks.length; index++) {
                 if (workbooks[index].workbook_name === workbook_name) {
@@ -56,27 +57,50 @@ router.get('/start', (req, res) => {
     zoho.get(`${currentWorkbook}?method=worksheet.records.fetch&worksheet_name=Old Prices`)
         .then(function (response) {
             oldPrices = response.data.records
-            console.log(oldPrices)
+            res.redirect('/liteoptec/continue')
         })
         .catch(function (error) {
             console.log('errorFG', error)
         })
 })
 
-router.get('/newStock', (req, res) => {
+router.get('/continue', (req, res) => {
     const zoho = axios.create({
         baseURL: `https://sheet.zoho.com/api/v2/`,
         timeout: 20000,
         headers: {'Authorization': `Zoho-oauthtoken ${token}`},
     })
-    zoho.get('kspsmb1b84b1d7c014acb8ed2ea1f2c374d47?method=worksheet.records.fetch&worksheet_name=New Stock')
+    zoho.get(`${currentWorkbook}?method=worksheet.records.fetch&worksheet_name=New Prices`)
         .then(function (response) {
-            newStock = response.data.records
-            res.redirect('/response')
+            newPrices = response.data.records
+            res.redirect('/liteoptec/end')
         })
         .catch(function (error) {
             console.log('errorFG', error)
         })
+})
+
+router.get('/end', (req, res) => {
+    if (oldPrices.length === newPrices.length) {
+        for (let i = 0; i < oldPrices.length; i++) {
+            for (let x = 0; x < newPrices.length; x++) {
+                if (oldPrices[i].ItemNumber === newPrices[x].ItemNumber) {
+                    if (oldPrices[i].Pricing !== newPrices[x].Pricing) {
+                        priceChanges.push(newPrices[x])
+                    }
+                }
+            }
+        }
+        res.render('compare', { length: true, change: true, prices: priceChanges })
+    } else {
+        for (let i = 0; i < oldStock.length; i++) {
+            if (newStock.includes(oldStock[i])) {
+                console.log("Item found")
+                newItems.push(oldStock[i])
+            }
+        }
+        res.render('compare', { length: false, change: false, prices: newItems })
+    }
 })
 
 module.exports = router
