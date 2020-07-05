a827fa8a65c7e3671778f4df7e092407590dc3b3
Gavin Fourie <gavinfourie07@gmail.com>
2020-07-05 16:53:26 +0200

    Fixed bugs

--- suppliers/liteoptec.js
+++ suppliers/liteoptec.js
@@ -3,11 +3,11 @@ let router = express.Router()
 let code = null
 let workbook_name = 'Lite Optec'
 
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
     res.redirect('https://accounts.zoho.com/oauth/v2/auth?response_type=code&client_id=1000.MAUUUZO4JJ0D5UOS7NA1XJA6EIJADH&scope=ZohoSheet.dataAPI.READ&redirect_uri=https://futurama-app.herokuapp.com/liteoptec/redirect')
 })
 
-app.get('/redirect', (req, res) => {
+router.get('/redirect', (req, res) => {
     code = req.query.code
     axios.post(`https://accounts.zoho.com/oauth/v2/token?code=${code}&grant_type=authorization_code&client_id=1000.MAUUUZO4JJ0D5UOS7NA1XJA6EIJADH&client_secret=a78690fdc6ecf1e65395b462e5e484833f0fab18d3&redirect_uri=https://futurama-app.herokuapp.com/liteoptec/redirect`)
         .then(function (response) {
@@ -19,7 +19,7 @@ app.get('/redirect', (req, res) => {
         })
 })
 
-app.get('/workbook', (req, res) => {
+router.get('/workbook', (req, res) => {
     const zoho = axios.create({
         baseURL: 'https://sheet.zoho.com/api/v2/',
         timeout: 20000,
