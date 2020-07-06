8d807e069774030059d7972ce78f7b7fc09f506e
Gavin Fourie <gavinfourie07@gmail.com>
2020-07-06 22:02:43 +0200

    Fixed bugs

--- suppliers/liteoptec.js
+++ suppliers/liteoptec.js
@@ -13,13 +13,14 @@ router.get('/', (req, res) => {
     oldPrices = []
     newPrices = []
     priceChanges = []
-    res.redirect('https://accounts.zoho.com/oauth/v2/auth?response_type=code&client_id=1000.MAUUUZO4JJ0D5UOS7NA1XJA6EIJADH&scope=ZohoSheet.dataAPI.READ,ZohoSheet.dataAPI.UPDATE,ZohoSheet.dataAPI.CREATE&redirect_uri=https://futurama-app.herokuapp.com/liteoptec/redirect')
+    res.redirect('https://accounts.zoho.com/oauth/v2/auth?response_type=code&client_id=1000.MAUUUZO4JJ0D5UOS7NA1XJA6EIJADH&scope=ZohoSheet.dataAPI.UPDATE,ZohoSheet.dataAPI.READ&redirect_uri=https://futurama-app.herokuapp.com/liteoptec/redirect')
 })
 
 router.get('/redirect', (req, res) => {
     code = req.query.code
     axios.post(`https://accounts.zoho.com/oauth/v2/token?code=${code}&grant_type=authorization_code&client_id=1000.MAUUUZO4JJ0D5UOS7NA1XJA6EIJADH&client_secret=a78690fdc6ecf1e65395b462e5e484833f0fab18d3&redirect_uri=https://futurama-app.herokuapp.com/liteoptec/redirect`)
         .then(function (response) {
+            console.log(response)
             token = response.data.access_token
             res.redirect('/liteoptec/workbook')
         })
