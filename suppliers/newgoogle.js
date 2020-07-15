d431a84c25e537edb50ae452340d6a070b16a360
Gavin Fourie <gavinfourie@Gavins-Air.Dlink>
2020-07-15 21:53:31 +0200

    Bug Fixes

--- suppliers/newgoogle.js
+++ suppliers/newgoogle.js
@@ -1,76 +1,20 @@
 const express = require('express');
-let router = express.Router()
-const fs = require('fs');
-const readline = require('readline');
-const {google} = require('googleapis');
-const request = require('request');
-
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
-
-const TOKEN_PATH = 'token.json';
+const multer = require('multer');
+const upload = multer({
+    dest: 'uploads/'
+});
+let router = express.Router();
 
 router.get('/', (req, res) => {
     res.render('indexg')
 })
 
-router.get('/get', (req, res) => {
-    fs.readFile('credentials.json', (err, content) => {
-        if (err) return console.log('Error loading client secret file:', err);
-    
-        authorize(JSON.parse(content), listMajors);
-    });
-    function authorize(credentials, callback) {
-        const {client_secret, client_id, redirect_uris} = credentials.installed;
-        const oAuth2Client = new google.auth.OAuth2(
-            client_id, client_secret, redirect_uris[0]);
-        
-        fs.readFile(TOKEN_PATH, (err, token) => {
-            if (err) return getNewToken(oAuth2Client, callback);
-            oAuth2Client.setCredentials(JSON.parse(token));
-            callback(oAuth2Client);
-        });
-    };
-    function getNewToken(oAuth2Client, callback) {
-        const authUrl = oAuth2Client.generateAuthUrl({
-            access_type: 'offline',
-            scope: SCOPES,
-        });
-        console.log('Authorize this app by visiting this url: ', authUrl);
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout,
-        });
-        rl.question('Enter the code from that page here: ', (code) => {
-            rl.close();
-            oAuth2Client.getToken(code, (err, token) => {
-                if (err) return console.log('Error while trying to receive access token', err);
-                oAuth2Client.setCredentials(token);
-                fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-                    if (err) return console.log(err);
-                    console.log('Token stored to', TOKEN_PATH);
-                });
-                callback(oAuth2Client);
-            });
-        });
-    }
-    function listMajors(auth) {
-        const sheets = google.sheets({version: 'v4', auth});
-        sheets.spreadsheets.values.get({
-            spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
-            range: 'Class DATA!A2:E',
-        }, (err, res) => {
-            if (err) return console.log('The API returned an error: ' + err);
-            const rows = res.data.values;
-            if (rows.length) {
-                console.log('Name, Major:');
-                rows.map((row) => {
-                    console.log(`${row[0]}, ${row[4]}`);
-                });
-            } else {
-                console.log('No data found.');
-            }
-        });
-    }
+router.post('/', upload.single('old-sheet'), (req, res) => {
+    res.redirect('/after')
+})
+
+router.get('/after', (req, res) => {
+    res.render('after')
 })
 
 module.exports = router
