e29f0902a20ea2bd47a323f4b2885fce735d5cd7
Gavin Fourie <gavinfourie@Gavins-Air.Dlink>
2020-07-16 21:38:16 +0200

    Bug fixes

--- suppliers/newgoogle.js
+++ suppliers/newgoogle.js
@@ -1,20 +1,178 @@
 const express = require('express');
-const multer = require('multer');
-const upload = multer({
-    dest: 'uploads/'
-});
 let router = express.Router();
+const formidable = require('formidable');
+const excel = require('xlsx-to-json-lc');
+const toexcel = require('node-excel-export');
+let OldSheet
+let NewSheet
+let priceChanges = []
+let newItems = []
 
 router.get('/', (req, res) => {
     res.render('indexg')
 })
 
-router.post('/', upload.single('old-sheet'), function (req, res, next) {
-    res.redirect('/newgoogle/after')
+router.post('/', (req, res, next) => {
+    const form = formidable({ multiples: true })
+    let newFile = null
+    let jsonRes = null
+
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            next(err)
+            return
+        }
+    })
+    form.on('file', (name, file) => {
+        newFile = file.path
+        excel({
+            input: newFile,
+            output: jsonRes,
+            lowerCaseHeaders: true
+        }, function(err, result) {
+            if (err) {
+                console.log(err)
+            } else {
+                OldSheet = result
+                res.redirect('/newgoogle/new')
+                console.log(result)
+            }
+        })
+    })
 })
 
-router.get('/after', (req, res) => {
-    res.render('after')
+router.get('/new', (req, res) => {
+    res.render('indexgn')
+})
+
+router.post('/new', (req, res, next) => {
+    const form = formidable({ multiples: true })
+    let newFile = null
+    let jsonRes = null
+
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            next(err)
+            return
+        }
+    })
+    form.on('file', (name, file) => {
+        newFile = file.path
+        excel({
+            input: newFile,
+            output: jsonRes,
+            lowerCaseHeaders: true
+        }, function(err, result) {
+            if (err) {
+                console.log(err)
+            } else {
+                NewSheet = result
+                res.redirect('/newgoogle/compare')
+                console.log(result)
+            }
+        })
+    })
+})
+
+router.get('/compare', (req, res) => {
+    if (OldSheet.length === NewSheet.length) {
+        for (let i = 0; i < OldSheet.length; i++) {
+            for (let x = 0; x < NewSheet.length; x++) {
+                if (OldSheet[i]['item number'] === NewSheet[x]['item number']) {
+                    if (OldSheet[i].pricing !== NewSheet[x].pricing) {
+                        priceChanges.push(NewSheet[x])
+                    }
+                }
+            }
+        }
+        const styles = {
+            headerDark: {
+              fill: {
+                fgColor: {
+                  rgb: 'FF000000'
+                }
+              },
+              font: {
+                color: {
+                  rgb: 'FFFFFFFF'
+                },
+                sz: 14,
+                bold: true,
+                underline: true
+              }
+            }
+        };
+        const specification = {
+            'item number': {
+                displayName: 'Item Number',
+                headerStyle: styles.headerDark,
+                width: 120
+            },
+            pricing: {
+                displayName: 'Pricing',
+                headerStyle: styles.headerDark,
+                width: 120
+            }
+        }
+        const sending = toexcel.buildExport(
+            [
+                {
+                    name: 'Export',
+                    specification: specification,
+                    data: priceChanges
+                }
+            ]
+        )
+        res.attachment('export.xlsx')
+        res.send(sending)
+    } else {
+        for (let i = 0; i < OldSheet.length; i++) {
+            if (NewSheet.includes(OldSheet[i])) {
+                console.log("Item found")
+                newItems.push(OldSheet[i])
+            }
+        }
+        const styles = {
+            headerDark: {
+              fill: {
+                fgColor: {
+                  rgb: 'FF000000'
+                }
+              },
+              font: {
+                color: {
+                  rgb: 'FFFFFFFF'
+                },
+                sz: 14,
+                bold: true,
+                underline: true
+              }
+            }
+        };
+        const specification = {
+            'item number': {
+                displayName: 'Item Number',
+                headerStyle: styles.headerDark,
+                width: 120
+            },
+            pricing: {
+                displayName: 'Pricing',
+                headerStyle: styles.headerDark,
+                width: 120
+            }
+        }
+        const sending = toexcel.buildExport(
+            [
+                {
+                    name: 'Exportf',
+                    specification: specification,
+                    data: newItems
+                }
+            ]
+        )
+        res.attachment('exportf.xlsx')
+        res.send(sending)
+    }
 })
 
 module.exports = router
