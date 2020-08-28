ef5c35464a0ca641440d9d4cb689c13887ee16f2
Gavin Fourie <gavinfourie07@gmail.com>
2020-08-28 19:06:01 +0200

    Newly added

--- suppliers/test.js
+++ suppliers/test.js
@@ -21,7 +21,7 @@ router.post('/', (req, res, next) => {
     let jsonRes = null
 
     form.parse(req, (err, fields, files) => {
-        console.log(files[0].path)
+        console.log(files['old-sheet'].path)
         newFile = files
         jsonRes = XLSX.readFile(newFile.path)
         // oldSheet = XLSX.utils.sheet_to_json(newFile, {header:"SKU"})
