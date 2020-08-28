afd743ebafc69c6543ca6eb215097d5a07616966
Gavin Fourie <gavinfourie07@gmail.com>
2020-08-28 21:25:20 +0200

    Newly added

--- suppliers/test.js
+++ suppliers/test.js
@@ -31,13 +31,13 @@ router.post('/', (req, res, next) => {
           let sheetName = jsonRes.SheetNames[ii]
           let sheet = jsonRes.Sheets[sheetName]
           let sheetToJson = XLSX.utils.sheet_to_json(sheet)
-          OldSheet = sheetToJson
+          OldSheet.append(sheetToJson)
           console.log(typeof OldSheet)
           ii += 1
         }
         // let sheet = jsonRes.Sheets[first_sheet]
         // OldSheet = XLSX.utils.sheet_to_json(sheet)
-        // console.log(OldSheet)
+        console.log(OldSheet)
         res.redirect('/test/new')
     })
 })
