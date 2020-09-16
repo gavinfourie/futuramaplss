bda5420d0f5e9e83a636e2cfd0e01138338bb114
Gavin Fourie <gavinfourie07@gmail.com>
2020-09-16 22:07:42 +0200

    new testing

--- suppliers/test.js
+++ suppliers/test.js
@@ -20,10 +20,14 @@ router.post('/', (req, res, next) => {
 
     form.parse(req, (err, fields, files) => {
         let workbook = XLSX.readFile(files['old-sheet'].path)
-        let first_sheet_name = workbook.SheetNames[0];
-        console.log(workbook.SheetNames.length);
-        let worksheet = workbook.Sheets[first_sheet_name];
-        //console.log(XLSX.utils.sheet_to_json(worksheet));
+        let wbl = workbook.SheetNames.length;
+        let worksheet;
+        let i = 0;
+        while (i < wbl) {
+          let current = workbook.SheetNames[i];
+          worksheet.push(workbook.Sheets[current])
+        }
+        console.log(XLSX.utils.sheet_to_json(worksheet));
         // res.attachment('export.xlsx')
         // res.send(book)
         //res.attachment('export.xlsx')
