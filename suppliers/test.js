626db594e820270c9fa9f8b9ad4a2252b28b20fc
Gavin Fourie <gavinfourie07@gmail.com>
2020-09-19 18:45:41 +0200

    new testing

--- suppliers/test.js
+++ suppliers/test.js
@@ -34,65 +34,59 @@ router.post('/', (req, res, next) => {
 })
 
 router.get('/new', (req, res) => {
-    res.render('indexgn')
+    res.render('indexgnt')
 })
 
 router.post('/new', (req, res, next) => {
-    const form = formidable({ multiples: true })
-    let newFile = null
-    let jsonRes = null
+  const form = new formidable.IncomingForm()
 
-    form.parse(req, (err, fields, files) => {
-        if (err) {
-            next(err)
-            return
+  form.parse(req, (err, fields, files) => {
+      let sfile = files['new-sheet'].path
+      let jfile = xtj({
+        sourceFile: sfile,
+        columnToKey: {
+          '*': '{{columnHeader}}'
         }
-    })
-    form.on('file', (name, file) => {
-        newFile = file.path
-        excel({
-            input: newFile,
-            output: jsonRes,
-            lowerCaseHeaders: true
-        }, function(err, result) {
-            if (err) {
-                console.log(err)
-            } else {
-                NewSheet = result
-                res.redirect('/main/compare')
-            }
-        })
-    })
+      })
+      for (var sheet in jfile) {
+        for (var item in jfile[sheet]) {
+          if (jfile[sheet][item]['Cost (ex VAT)'] > 0){
+              oldItems.push(jfile[sheet][item])
+          }
+        }
+      }
+      res.redirect('/test/compare')
+  })
 })
 
 router.get('/compare', (req, res) => {
     let oldNumbers = []
-    for (let i = 0; i < OldSheet.length; i++) {
-        let itemCode = OldSheet[i]['sku']
+    for (let i = 0; i < oldItems.length; i++) {
+        let itemCode = oldItems[i]['SKU']
         oldNumbers.push(itemCode)
     }
     let newNumbers = []
-    for (let i = 0; i < NewSheet.length; i++) {
-        let itemCode = NewSheet[i]['sku']
+    for (let i = 0; i < newItems.length; i++) {
+        let itemCode = newItems[i]['SKU']
         newNumbers.push(itemCode)
     }
     const dropped = _.difference(oldNumbers, newNumbers)
     const droppedFinal = []
     for (let i = 0; i < dropped.length; i++) {
-        let item = { 'sku': dropped[i] }
+        let item = { 'SKU': dropped[i] }
         droppedFinal.push(item)
     }
     const added = _.difference(newNumbers, oldNumbers)
     const addedFinal = []
     for (let i = 0; i < added.length; i++) {
-        let item = { 'sku': added[i] }
+        let item = { 'SKU': added[i] }
         addedFinal.push(item)
     }
-    for (let i = 0; i < OldSheet.length; i++) {
-        for (let x = 0; x < NewSheet.length; x++) {
-            if (OldSheet[i]['sku'] === NewSheet[x]['sku']) {
-                if (OldSheet[i]['cost ex vat'] !== NewSheet[x]['cost ex vat']) {
-                    priceChanges.push(NewSheet[x])
+    for (let i = 0; i < oldItems.length; i++) {
+        for (let x = 0; x < newItems.length; x++) {
+            if (oldItems[i]['SKU'] === newItems[x]['SKU']) {
+                if (oldItems[i]['Cost (ex VAT)'] !== newItems[x]['Cost (ex VAT)']) {
+                    priceChanges.push(newItems[x])
                 }
             }
         }
@@ -115,20 +109,20 @@ router.get('/compare', (req, res) => {
         }
     };
     const specificationDA = {
-        'sku': {
+        'SKU': {
             displayName: 'SKU',
             headerStyle: styles.headerDark,
             width: 120
         }
     }
     const specification = {
-        'sku': {
+        'SKU': {
             displayName: 'SKU',
             headerStyle: styles.headerDark,
             width: 120
         },
-        'cost ex vat': {
-            displayName: 'Cost ex VAT',
+        'Cost (ex VAT)': {
+            displayName: 'Cost (ex VAT)',
             headerStyle: styles.headerDark,
             width: 120
         }
