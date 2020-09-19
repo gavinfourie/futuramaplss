4f7be0e705a587ebb4c5e163383f4af934bd7b78
Gavin Fourie <gavinfourie07@gmail.com>
2020-09-19 20:53:47 +0200

    new testing

--- suppliers/test.js
+++ suppliers/test.js
@@ -69,7 +69,7 @@ router.get('/compare', (req, res) => {
     for (let i = 0; i < oldItems.length; i++) {
         let itemCode = oldItems[i]['SKU']
         for (let i = 0; i < oldNumbers.length; i++) {
-          if (itemCode === oldNumbers[i]) {
+          if (itemCode === oldNumbers[i]['SKU']) {
             duplicates.push(itemCode)
           } else {
             oldNumbers.push(itemCode)
@@ -80,7 +80,7 @@ router.get('/compare', (req, res) => {
     for (let i = 0; i < newItems.length; i++) {
         let itemCode = newItems[i]['SKU']
         for (let i = 0; i < newNumbers.length; i++) {
-          if (itemCode === newNumbers[i]) {
+          if (itemCode === newNumbers[i]['SKU']) {
             duplicates.push(itemCode)
           } else {
             newNumbers.push(itemCode)
