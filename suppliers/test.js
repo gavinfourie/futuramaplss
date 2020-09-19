babbb6a3e1b364943ac4589406939a07160a82b3
Gavin Fourie <gavinfourie07@gmail.com>
2020-09-19 17:19:10 +0200

    new testing

--- suppliers/test.js
+++ suppliers/test.js
@@ -21,13 +21,13 @@ router.post('/', (req, res, next) => {
 
     form.parse(req, (err, fields, files) => {
         let sfile = files['old-sheet'].path
-        col1 = "A"
-        col2 = "C"
+        col1 = 'A'
+        col2 = 'C'
         let jfile = xtj({
           sourceFile: sfile,
           columnToKey: {
-            col1: 'SKU',
-            col2: 'Cost (ex VAT)'
+            'col1': 'SKU',
+            'col2': 'Cost (ex VAT)'
           }
         })
         //res.attachment('export.xlsx')
