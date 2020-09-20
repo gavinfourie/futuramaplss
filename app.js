9b72df7233ce7de3f34953e7fa066a4369d86f0d
Gavin Fourie <gavinfourie07@gmail.com>
2020-09-20 07:48:46 +0200

    almost

--- app.js
+++ app.js
@@ -11,27 +11,4 @@ app.get('/', (req, res) => {
     res.render('home', {text: 'Welcome!'})
 })
 
-app.get('/doCompare', (req, res) => {
-    if (oldStock.length === newStock.length) {
-        for (let i = 0; i < oldStock.length; i++) {
-            for (let x = 0; x < newStock.length; x++) {
-                if (oldStock[i].ItemNumber === newStock[x].ItemNumber) {
-                    if (oldStock[i].Pricing !== newStock[x].Pricing) {
-                        priceChanges.push(newStock[x])
-                    }
-                }
-            }
-        }
-        res.render('compare', { length: true, change: true, prices: priceChanges })
-    } else {
-        for (let i = 0; i < oldStock.length; i++) {
-            if (newStock.includes(oldStock[i])) {
-                console.log("Item found")
-                newItems.push(oldStock[i])
-            }
-        }
-        res.render('compare', { length: false, change: false, prices: newItems })
-    }
-})
-
 app.listen(process.env.PORT || 3000)
