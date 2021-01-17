70c82ed8998100599f6e2a60c29ab2697c477c0c
Gavin Fourie <gavinfourie07@gmail.com>
2021-01-17 19:06:59 +0200

    Date functionality bug fixes

--- workers/stocksheets.js
+++ workers/stocksheets.js
@@ -102,7 +102,7 @@ router.get('/compare', (req, res) => {
     }
     let myDateMonth = DateTime.local().month
     if (!myDateMonth.length > 1) {
-        myDateMonth = '0' + myDateMonth
+        myDateMonth = "0" + myDateMonth
     }
     let myDateYear = DateTime.local().year
     console.log("Day: ", myDateDay)
