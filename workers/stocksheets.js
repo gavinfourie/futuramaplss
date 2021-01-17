58227a494b65fc43564b29470e95459b1612e69b
Gavin Fourie <gavinfourie07@gmail.com>
2021-01-17 17:04:43 +0200

    Testing

--- workers/stocksheets.js
+++ workers/stocksheets.js
@@ -35,7 +35,7 @@ router.post('/', (req, res, next) => {
             console.log(json[i]);
         }*/
         for (var item in jfile) {
-            console.log("Checking", jfile[item]['In Stock'])
+            console.log("Checking", jfile[item])
             // Read all in stock items from Magento into an Array
             if (jfile[item]['In Stock'] === 'In Stock'){
                 magentoInStock.push(jfile[item])
