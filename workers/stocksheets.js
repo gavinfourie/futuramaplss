af970da9c19b761d53e07c8ea3011a2eb72501bc
Gavin Fourie <gavinfourie07@gmail.com>
2021-04-09 09:59:18 +0200

    last minute changes

--- workers/stocksheets.js
+++ workers/stocksheets.js
@@ -180,6 +180,7 @@ router.get('/compare', (req, res) => {
         }
     }
     let finalOut = _.uniqBy(changeToOut, 'SKU')
+    console.log("Out of stock", finalOut)
     // Make arrays of no duplicates
     let magentoSKU = _.uniqBy(magentoInStock, 'SKU')
     let dearSKU = _.uniqBy(dearInStock, 'SKU')
