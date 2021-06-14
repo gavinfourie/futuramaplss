e32fdf4b43e9b10554438fe0dc3c5faa17f3311d
gavin <gavinfourie07@gmail.com>
2021-06-14 23:02:59 +0200

    Bug fixes

--- workers/stocksheetcompare.js
+++ workers/stocksheetcompare.js
@@ -193,7 +193,6 @@ router.get('/compare', (req, res) => {
     }
     let protoDylanInStock = _.uniqBy(protoAllInStock, 'SKU')
     let inStock = _.differenceBy(protoDylanInStock, protoMagentoInStock, 'SKU')
-    console.log(inStock)
     let tempChangeToIn = []
     for (let i = 0; i < inStock.length; i++) {
         if (inStock[i]['Product Name']) {
